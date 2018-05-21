72557bd9c67132e8f5c0bc022ab4a660751b402c
Sergey Ryabov <colriot@gmail.com>
2018-05-21 22:38:17 +0300

    Refactor MainActivity logic and UI into MainFragment

--- app/src/main/java/spb/android/academy/fragments/MainActivity.java
+++ app/src/main/java/spb/android/academy/fragments/MainActivity.java
@@ -1,58 +1,19 @@
 package spb.android.academy.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.List;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import spb.android.academy.fragments.domain.Collection;
-import spb.android.academy.fragments.network.NetworkManager;
 
 public class MainActivity extends AppCompatActivity {
-
-  private ViewPager viewPager;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    viewPager = findViewById(R.id.activity_main_view_pager);
 
-    NetworkManager.getApiInstance().getFeaturedCollections().enqueue(new Callback<List<Collection>>() {
-      @Override
-      public void onResponse(@NonNull Call<List<Collection>> call, @NonNull Response<List<Collection>> response) {
-        List<Collection> collections = response.body();
-        if (collections != null) {
-          for (Collection collection : collections) {
-            CollectionsRepository.getInstance().save(collection);
-          }
-          setupViewPagerAdapter();
-        } else {
-          showErrorMessage();
-        }
-      }
-
-      @Override public void onFailure(@NonNull Call<List<Collection>> call, @NonNull Throwable t) {
-        showErrorMessage();
-      }
-    });
-  }
-
-  private void setupViewPagerAdapter() {
-    final List<Collection> collections = CollectionsRepository.getInstance().getAll();
-    final List<CollectionFragment> fragments = new ArrayList<>();
-    for (Collection collection : collections) {
-      fragments.add(CollectionFragment.newInstance(collection.getId()));
+    if (savedInstanceState == null) {
+      getSupportFragmentManager()
+          .beginTransaction()
+          .replace(R.id.content, MainFragment.newInstance())
+          .commit();
     }
-    final CollectionsPagerAdapter adapter = new CollectionsPagerAdapter(getSupportFragmentManager(), fragments);
-    viewPager.setAdapter(adapter);
-  }
-
-  private void showErrorMessage() {
-    Toast.makeText(this, "Something went wrong :(", Toast.LENGTH_SHORT).show();
   }
 }
